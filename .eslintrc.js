3bd34bd35954cc30817d9ccf8e9bd30d35127312
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-07-31 19:55:32 +0300

    gata pentru azi

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,18 +6,21 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
+    ecmaVersion: 2024, // Sau cea mai recentă versiune ECMAScript dorită adică era scris 12 (versiunea 12)
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'react/react-in-jsx-scope': 'off',
+    // Alte reguli personalizate aici
   },
 };
--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import './article.css';
 
 const Article = () => {
-    return (
-        <div>Article</div>
-    )
-}
+  return <div>Article</div>;
+};
 
-export default Article
+export default Article;
