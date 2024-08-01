aeda022f9b09dc0e983b1eb6167796720aaf9ec0
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-08-01 18:52:20 +0300

    cam aci pentru azi

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './possibility.css';
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image"></div>
+    </div>
+  );
 };
 
 export default Possibility;
