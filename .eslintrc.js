1286ef2983fbf68632daa1f76dbd31220f0f3d3a
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-08-01 11:19:13 +0300

    ultima modificare pentru eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,17 +7,21 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended', // Aceasta include 'eslint-config-prettier' automat
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2024, // Sau cea mai recentă versiune ECMAScript dorită adică era scris 12 (versiunea 12)
+    ecmaVersion: 'latest', // Sau cea mai recentă versiune ECMAScript dorită (2024,)
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier', // Adăugăm pluginul prettier aici
+  ],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'react/react-in-jsx-scope': 'off',
