03c963db1fea24053a0ca323dfd4e071f071b1fb
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-08-12 19:00:27 +0300

    gata pentru azi

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types'; // Importă PropTypes
 import './article.css';
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ imgUrl, date, title }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+};
+
+// Adaugă validarea cu PropTypes
+Article.propTypes = {
+  imgUrl: PropTypes.string.isRequired, // imgUrl trebuie să fie un string și este obligatoriu
+  date: PropTypes.string.isRequired, // date trebuie să fie un string și este obligatoriu
+  title: PropTypes.string.isRequired, // title trebuie să fie un string și este obligatoriu
 };
 
 export default Article;
